cadc71b931d3ce3e7c9b4dbf13999496b0d1bd43
Érica Alves <erica.asnt@hotmail.com>
2026-08-14 22:36:52 -0300

    feat: Worker com config tipada RabbitMq e fail-fast

--- ProcessadorImpressao/src/Processador.Worker/Program.cs
+++ ProcessadorImpressao/src/Processador.Worker/Program.cs
@@ -1,7 +1,37 @@
 using Processador.Worker;
+using Processador.Worker.Application.Idempotency;
+using Processador.Worker.Consumers;
+using Processador.Worker.Infrastructure.Idempotency;
+using RabbitMQ.Client;
+using Shared.Configuration;
 
 var builder = Host.CreateApplicationBuilder(args);
+
+var rabbitMq = builder.Configuration
+    .GetSection(RabbitMqOptions.SectionName)
+    .Get<RabbitMqOptions>()
+    ?? new RabbitMqOptions();
+
+rabbitMq.Validar();
+
+var factory = new ConnectionFactory
+{
+    HostName = rabbitMq.HostName,
+    UserName = rabbitMq.UserName,
+    Password = rabbitMq.Password
+};
+
 builder.Services.AddHostedService<Worker>();
+builder.Services.AddSingleton<IProcessedMessageStore, InMemoryProcessedMessageStore>();
+builder.Services.AddSingleton(_ => factory.CreateConnection());
+builder.Services.AddSingleton(sp => sp.GetRequiredService<IConnection>().CreateModel());
+builder.Services.AddSingleton<PedidoImpressaoConsumer>(sp => new PedidoImpressaoConsumer(
+    sp.GetRequiredService<IModel>(),
+    sp.GetRequiredService<IProcessedMessageStore>(),
+    sp.GetRequiredService<ILogger<PedidoImpressaoConsumer>>()));
 
 var host = builder.Build();
+
+host.Services.GetRequiredService<PedidoImpressaoConsumer>().IniciarConsumo();
+
 host.Run();
